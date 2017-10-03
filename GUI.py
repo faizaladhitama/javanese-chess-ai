09ca95e425c8af5739ef48c12a75e423ec2c51be
Tinna Fauziah Azhar <tinnafauziah@gmail.com>
2017-10-03 11:39:07 +0700

    Edit GUI

--- GUI.py
+++ GUI.py
@@ -2,6 +2,18 @@ from tkinter import *
 from PIL import Image,ImageTk
 import os
 
+#Batas pion paling kiri
+PION_LEFT_MAX_X0 = 370
+
+#Batas pion paling kanan
+PION_RIGHT_MAX_X1 = 970
+
+#Batas pion paling atas
+PION_UP_MAX_Y0 = 50
+
+#Batas pion paling bawah
+PION_DOWN_MAX_Y1 = 650
+
 class GUI : 
 	def __init__(self, master):
 		self.frame = Canvas(master, bg = "#7BC5DA", width = master.winfo_screenwidth(), height = master.winfo_screenheight())
@@ -24,41 +36,57 @@ class GUI :
 		BluePion = Pion(self.frame,"blue")
 
 		#bikin Edgenya
-		edge1 = theLine.create_line(390,70,670,350) #\ atas ke tengah
-		edge2 = theLine.create_line(670,350,950,630) #\ tengah ke bawah
+		theLine.create_line(390,70,670,350) #\ atas ke tengah
+		theLine.create_line(670,350,950,630) #\ tengah ke bawah
 
-		edge3 = theLine.create_line(670,70,670,350) #| atas ke tengah
-		edge4 = theLine.create_line(670,350,670,630) #| tengah ke bawah
+		theLine.create_line(670,70,670,350) #| atas ke tengah
+		theLine.create_line(670,350,670,630) #| tengah ke bawah
 
-		edge5 = theLine.create_line(950,70,670,350) #/ atas ke tengah
-		edge6 = theLine.create_line(670,350,390,630) #/ tengah ke bawah
+		theLine.create_line(950,70,670,350) #/ atas ke tengah
+		theLine.create_line(670,350,390,630) #/ tengah ke bawah
 
-		edge7 = theLine.create_line(390,350,670,350) # - kiri ke tengah
-		edge8 = theLine.create_line(670,350,950,350) #- tengah ke kanan
+		theLine.create_line(390,350,670,350) # - kiri ke tengah
+		theLine.create_line(670,350,950,350) #- tengah ke kanan
 
 		#Bikin Node (yang lingkaran kuning kecil)
-		node1 = theNode.create_node(375,55,405,85) #node kiri atas
-		node2 = theNode.create_node(655,55,685,85) #node tengah atas
-		node3 = theNode.create_node(935,55,965,85) #node kanan atas
+		theNode.create_node(375,55,405,85) #node kiri atas
+		theNode.create_node(655,55,685,85) #node tengah atas
+		theNode.create_node(935,55,965,85) #node kanan atas
 
-		node4 = theNode.create_node(375,335,405,365) #node kiri tengah 
-		node5 = theNode.create_node(655,335,685,365) #node tengah tengah
-		node6 = theNode.create_node(935,335,965,365) #node kanan tengah 
+		theNode.create_node(375,335,405,365) #node kiri tengah 
+		theNode.create_node(655,335,685,365) #node tengah tengah
+		theNode.create_node(935,335,965,365) #node kanan tengah 
 
-		node7 = theNode.create_node(375,615,405,645) #node kiri bawah 
-		node8 = theNode.create_node(655,615,685,645) #node tengah bawah
-		node9 = theNode.create_node(935,615,965,645) #node kanan bawah
+		theNode.create_node(375,615,405,645) #node kiri bawah 
+		theNode.create_node(655,615,685,645) #node tengah bawah
+		theNode.create_node(935,615,965,645) #node kanan bawah
 
 		#bikin pion2nya
-		pinkPion1 = PinkPion.create_pion(370,50,410,90)
-		pinkPion2 = PinkPion.create_pion(650,50,690,90)
-		pinkPion3 = PinkPion.create_pion(930,50,970,90)
+		PinkPion.create_pion(370,50,410,90)
+		PinkPion.create_pion(650,50,690,90)
+		PinkPion.create_pion(930,50,970,90)
 
-		bluePion1 = BluePion.create_pion(370,610,410,650)
-		bluePion2 = BluePion.create_pion(650,610,690,650)
-		bluePion3 = BluePion.create_pion(930,610,970,650)
+		BluePion.create_pion(370,610,410,650)
+		BluePion.create_pion(650,610,690,650)
+		BluePion.create_pion(930,610,970,650)
+
+		#bind_Event_Listener
+		self.frame.bind('<Enter>',self.pionOnClick)
 
 	#buat ngepack frame
+	def pionOnClick(self,event):
+		flagOnClick = 0
+		if(event.x <= PION_LEFT_MAX_X0 and event.x >= PION_RIGHT_MAX_X1 and event.y <= PION_UP_MAX_Y0 and event.y >= PION_DOWN_MAX_Y1):
+			print("terklik luar board")
+			print(event.x)
+			print(event.y)
+			self.frame.create_oval(event.x,event.x+10,event.y,event.y+10, fill = "#FBE9AD", outline = "#FBE9AD") 
+		else:
+			print("terklik dalam")
+			print(event.x)
+			print(event.y)
+			self.frame.create_oval(event.x,event.x+10,event.y,event.y+10, fill = "#FBE9AD", outline = "#FBE9AD") 
+
 	def packed(self):
 		self.master.minsize(width=self.master.winfo_screenwidth(), height=self.master.winfo_screenheight())
 		self.master.maxsize(width=self.master.winfo_screenwidth(), height=self.master.winfo_screenheight())
@@ -98,16 +126,13 @@ class Pion :
 		self.x1 = 0
 		self.y1 = 0
 
-	def pionOnClick(event):
-		print("terklik")
-
 	def create_pion(self, x0, y0, x1, y1):
 		self.set_initialPlace(x0, y0, x1, y1)
 		#kondisi warna kalo biru warna pionnya biru
 		if(self.color == "blue"):
-			self.frame.create_oval(x0, y0, x1, y1, fill = "#8799CB", outline = "#4E586C", width = 5).bind(pionOnClick)
+			self.frame.create_oval(x0, y0, x1, y1, fill = "#8799CB", outline = "#4E586C", width = 5)
 		elif(self.color == "pink"):
-			self.frame.create_oval(x0, y0, x1, y1, fill = "#E3ADC5", outline = "#66525B", width = 5).bind(pionOnClick)
+			self.frame.create_oval(x0, y0, x1, y1, fill = "#E3ADC5", outline = "#66525B", width = 5)
 
 	#buat ganti posisi awal (defaultnya kan 0)
 	def set_initialPlace(self, x0, y0, x1, y1):
@@ -120,7 +145,7 @@ class Pion :
 	def move_Up(self):
 		up_Y0 = self.y0 - 280
 		up_Y1 = self.y1 - 280
-		if(up_Y0 < 50):
+		if(up_Y0 < PION_UP_MAX_Y0):
 			print("Unable to move")
 		else:
 			self.y0 = up_Y0
@@ -130,7 +155,7 @@ class Pion :
 	def move_Down(self):
 		down_Y0 = self.y0 + 280
 		down_Y1 = self.y1 + 280
-		if(down_Y1 > 640):
+		if(down_Y1 > PION_DOWN_MAX_Y1):
 			print("Unable to move")
 		else:
 			self.y0 = down_Y0
@@ -140,7 +165,7 @@ class Pion :
 	def move_Left(self):
 		left_X0 = self.x0 - 280
 		left_X1 = self.x1 - 280
-		if(left_X0 < 370):
+		if(left_X0 < PION_LEFT_MAX_X0):
 			print("Unable to move")
 		else:
 			self.x0 = left_x0
@@ -150,7 +175,7 @@ class Pion :
 	def move_Right(self):
 		right_X0 = self.x0 + 280
 		right_X1 = self.x1 + 280
-		if(right_X1 > 960):
+		if(right_X1 > PION_RIGHT_MAX_X1):
 			print("Unable to move")
 		else:
 			self.x0 = right_x0
@@ -163,7 +188,7 @@ class Pion :
 		diagonalUpLeft_Y0 = self.y0 - 280
 		diagonalUpLeft_Y1 = self.y1 - 280
 
-		if(diagonalUpLeft_X0 < 370 or diagonalUpLeft_Y0 < 50):
+		if(diagonalUpLeft_X0 < PION_LEFT_MAX_X0 or diagonalUpLeft_Y0 < PION_UP_MAX_Y0):
 			print("Unable to move")
 		else:
 			self.x0 = diagonalUpLeft_X0
@@ -178,7 +203,7 @@ class Pion :
 		diagonalUpRight_Y0 = self.y0 - 280
 		diagonalUpRight_Y1 = self.y1 - 280
 
-		if(diagonalUpRight_X1 > 960 or diagonalUpRight_Y0 < 50):
+		if(diagonalUpRight_X1 > PION_RIGHT_MAX_X1 or diagonalUpRight_Y0 < PION_UP_MAX_Y0):
 			print("Unable to move")
 		else:
 			self.x0 = diagonalUpRight_X0
@@ -193,13 +218,13 @@ class Pion :
 		diagonalDownLeft_Y0 = self.y0 + 280
 		diagonalDownLeft_Y1 = self.y1 + 280
 
-		if(diagonalDownLeft_X0 < 370 or diagonalDownLeft_Y0 > 640):
+		if(diagonalDownLeft_X0 < PION_LEFT_MAX_X0 or diagonalDownLeft_Y1 > PION_DOWN_MAX_Y1):
 			print("Unable to move")
 		else:
-			self.x0 = diagonalUpLeft_X0
-			self.x1 = diagonalUpLeft_X1
-			self.y0 = diagonalUpLeft_Y0
-			self.y1 = diagonalUpLeft_Y1
+			self.x0 = diagonalDownLeft_X0
+			self.x1 = diagonalDownLeft_X1
+			self.y0 = diagonalDownLeft_Y0
+			self.y1 = diagonalDownLeft_Y1
 
 	#jalan diagonal bawah kanan
 	def move_DiagonalDownRight(self):
@@ -208,13 +233,13 @@ class Pion :
 		diagonalDownRight_Y0 = self.y0 + 280
 		diagonalDownRight_Y1 = self.y1 + 280
 
-		if(diagonalDownRight_X1 > 960 or diagonalDownRight_Y1 > 640):
+		if(diagonalDownRight_X1 > PION_RIGHT_MAX_X1 or diagonalDownRight_Y1 > PION_DOWN_MAX_Y1):
 			print("Unable to move")
 		else:
-			self.x0 = diagonalUpRight_X0
-			self.x1 = diagonalUpRight_X1
-			self.y0 = diagonalUpRight_Y0
-			self.y1 = diagonalUpRight_Y1
+			self.x0 = diagonalDownRight_X0
+			self.x1 = diagonalDownRight_X1
+			self.y0 = diagonalDownRight_Y0
+			self.y1 = diagonalDownRight_Y1
 
 if __name__ == '__main__':
 	root = Tk()
