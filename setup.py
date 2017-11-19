cbad0667b804c789961eb78c9bd7879bf1e9801b
faizal.adhitama <faizal.adhitama@ui.ac.id>
2017-11-19 09:29:16 +0700

    Add author

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ elif sys.platform == "win64":
 setup(
     name = "Catur Jawa",
     version = "0.5",
+    author="Faizal Adhitama Prabowo & Tinna Fauzia Azhar",
     description = "Catur Jawa Game",
     options = {"build_exe": build_exe_options},
     executables = [Executable("GUI.py", base=base)])
