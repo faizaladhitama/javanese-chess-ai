b3575de94c8e2254cfa70a9325809499c99f1428
faizal.adhitama <faizal.adhitama@ui.ac.id>
2017-11-23 12:48:21 +0700

    Renew test method

--- Test_Program.py
+++ Test_Program.py
@@ -55,96 +55,96 @@ class BoardTestCase(unittest.TestCase):
                     assert self._board.get_node_list()[i].get_occupied() == False
 
     def test_possible_move(self):
-        assert self._board.possible_move(self._board.select_node(0)) == ['7', '8']
+        assert self._board.possible_move(self._board.select_node(0)) == ['3', '4']
 
     def test_transition(self):
         self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
         self.test_node_assigned()
 
     def test_column_win(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(6))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(1))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(6))
         self._board.pawn_transition(self._board.select_node(4), self._board.select_node(5))
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(1))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(7))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(8))
         self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
         assert self._board.check_column() == True
 
     def test_row_win(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(5))
         assert self._board.check_row() ==  True
 
     def test_diagonal_win(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(6))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(8))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(6))
+        self._board.pawn_transition(self._board.select_node(4), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(4))
         assert self._board.check_diagonal() == True
 
     def test_utility_human_win_diagonal(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(6))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(8))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(6))
+        self._board.pawn_transition(self._board.select_node(4), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(4))
         assert self._board.utility() == -1
 
     def test_utility_human_win_column(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(6))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(1))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(6))
         self._board.pawn_transition(self._board.select_node(4), self._board.select_node(5))
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(1))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(7))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(8))
         self._board.pawn_transition(self._board.select_node(1), self._board.select_node(0))
         assert self._board.utility() == -1
 
     def test_utility_human_win_row(self):
-        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(0), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(5))
         assert self._board.utility() == -1
 
     def test_utility_neither_player_win(self):
         assert self._board.utility() == 0
 
     def test_utility_ai_win_row(self):
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(4), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(3))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(5))
         assert self._board.utility() == 1
 
     def test_utility_ai_win_column(self):
-        self._board.pawn_transition(self._board.select_node(4), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(1), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(8))
         self._board.pawn_transition(self._board.select_node(2), self._board.select_node(1))
-        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(5))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(7))
-        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(2))
+        self._board.pawn_transition(self._board.select_node(6), self._board.select_node(7))
         self._board.pawn_transition(self._board.select_node(4), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(4))
+        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(2))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(8))
         assert self._board.utility() == 1
 
     def test_utility_ai_win_diagonal(self):
-        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(8))
-        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(7))
+        self._board.pawn_transition(self._board.select_node(2), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(4))
         self._board.pawn_transition(self._board.select_node(4), self._board.select_node(3))
-        self._board.pawn_transition(self._board.select_node(3), self._board.select_node(2))
-        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(8))
+        self._board.pawn_transition(self._board.select_node(8), self._board.select_node(5))
+        self._board.pawn_transition(self._board.select_node(5), self._board.select_node(2))
+        self._board.pawn_transition(self._board.select_node(7), self._board.select_node(4))
         assert self._board.utility() == 1
 
 
